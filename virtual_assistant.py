1ff33b5432b3a06596f798356b8d846ddc2d7cf8
Emperor-Nathan <38165162+Emperor-Nathan@users.noreply.github.com>
2018-04-07 15:15:42 -0700

    Email and translate functions

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -1,12 +1,15 @@
 l_news=['news']
 l_music=['music']
 l_email=['email']
+l_translate['translate']
 def news():
     print('Okay, here is the news of today: [INSERT NEWS HERE]')
 def music():
     print('Okay, now playing music.')
 def email():
     print('Okay, here is your most recent email: [INSERT EMAIL HERE]')
+def translate():
+    print('[INSERT TRANSLATED TEXT HERE]')
 def search(list,string):
     n=len(list)-1
     while n>=0:
@@ -20,30 +23,34 @@ def listnames(list,string):
         while n<len(list):
             print(list[n])
             n+=1
-print('Welcome! I am your virtual assistant.\nType "new command" to define a new name for a command.\nType "list commands" to list all the names for a certain command.')
+def new(l,s,t):
+    if search(l,t):
+        l.append(s)
+print('Welcome! I am your virtual assistant.\nAvailable commands: news | music | email\nType "new command" to define a new name for a command.\nType "list commands" to list all the names for a certain command.')
 list=[l_news,l_music,l_email]
 while True:
     inp=input('')
     if inp=='new command':
         s=input('What is the name of the command: ')
         t=input('What does the command do: ') #insert an already-existing name for the specified command
-        if search(l_news,t):
-            l_news.append(s)
-        elif search(l_music,t):
-            l_music.append(s)
-        elif search(l_email,t):
-            l_email.append(s)
+        new(l_news,s,t)
+        new(l_music,s,t)
+        new(l_email,s,t)
+        new(l_translate,s,t)
     elif inp=='list commands': #This lists all the names for a certain command
         s=input('What is the name of the command: ') #input a name for the command
         listnames(l_news,s)
         listnames(l_music,s)
         listnames(l_email,s)
+        listnames(l_translate,s)
     elif search(l_news,inp):
         news()
     elif search(l_music,inp):
         music()
     elif search(l_email,inp):
         email()
+    elif search(l_translate,inp):
+        translate()
     else:
         print('That is not a valid input.')
 # assistant
